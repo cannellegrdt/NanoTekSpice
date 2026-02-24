8107e8561a3af000c4fa7db4b4585cd1a1e15ad9
aurelien <aurelien.demeusy@epitech.eu>
2026-02-24 16:57:24 +0100

    [ADD] documentation and doxyfile

--- include/nts/Parser.hpp
+++ include/nts/Parser.hpp
@@ -18,9 +18,21 @@ namespace nts {
 class Circuit;
 class IComponentFactory;
 
+/**
+ * @brief Parser class for parsing .nts files and creating circuits
+ */
 class Parser {
 public:
+  /**
+   * @brief Construct a new Parser object
+   *
+   * @param factory The component factory used to create components
+   */
   explicit Parser(IComponentFactory &factory);
+
+  /**
+   * @brief Destroy the Parser object
+   */
   ~Parser() = default;
 
   Parser(const Parser &) = delete;
@@ -29,7 +41,20 @@ public:
   Parser(Parser &&) noexcept = default;
   Parser &operator=(Parser &&) noexcept = delete;
 
+  /**
+   * @brief Parse a circuit from a file
+   *
+   * @param filepath The path to the file to parse
+   * @return Circuit The parsed circuit
+   */
   Circuit parse(const std::string &filepath) const;
+
+  /**
+   * @brief Parse a circuit from an input stream
+   *
+   * @param input The input stream to parse from
+   * @return Circuit The parsed circuit
+   */
   Circuit parse(std::istream &input) const;
 
 private:
@@ -38,19 +63,60 @@ private:
   mutable Lexer *_lexer = nullptr;
   mutable Token _currentToken;
 
+  /**
+   * @brief Advance the lexer to the next token
+   */
   void advance() const;
+
+  /**
+   * @brief Get the current token without consuming it
+   *
+   * @return const Token& The current token
+   */
   const Token &peek() const;
+
+  /**
+   * @brief Consume the current token if it matches the expected type
+   *
+   * @param expectedType The expected type of the token
+   * @return Token The consumed token
+   */
   Token eat(TokenType expectedType) const;
 
+  /**
+   * @brief Parse the chipsets section of the file
+   *
+   * @param circuit The circuit to add the chipsets to
+   */
   void parseChipsets(Circuit &circuit) const;
+
+  /**
+   * @brief Parse the links section of the file
+   *
+   * @param circuit The circuit to add the links to
+   */
   void parseLinks(Circuit &circuit) const;
 
+  /**
+   * @brief Parse a single chipset line
+   *
+   * @param circuit The circuit to add the chipset to
+   */
   void parseChipsetLine(Circuit &circuit) const;
+
+  /**
+   * @brief Parse a single link line
+   *
+   * @param circuit The circuit to add the link to
+   */
   void parseLinkLine(Circuit &circuit) const;
 
+  /**
+   * @brief Consume tokens until a newline is reached
+   */
   void eatUntilNewline() const;
 };
 
-}
+} // namespace nts
 
 #endif /* !NTS_PARSER_HPP_ */
--- include/nts/TristateLogic.hpp
+++ include/nts/TristateLogic.hpp
@@ -12,20 +12,79 @@
 
 namespace nts {
 
-    class TristateLogic {
-    public:
-        TristateLogic() = delete;
-        ~TristateLogic() = delete;
+/**
+ * @brief Helper class containing static methods for operations on Tristate
+ * values
+ */
+class TristateLogic {
+public:
+  TristateLogic() = delete;
+  ~TristateLogic() = delete;
 
-        static Tristate andGate(Tristate a, Tristate b) noexcept;
-        static Tristate orGate(Tristate a, Tristate b) noexcept;
-        static Tristate xorGate(Tristate a, Tristate b) noexcept;
-        static Tristate notGate(Tristate a) noexcept;
-        static Tristate nandGate(Tristate a, Tristate b) noexcept;
-        static Tristate norGate(Tristate a, Tristate b) noexcept;
-        static Tristate xnorGate(Tristate a, Tristate b) noexcept;
-    };
+  /**
+   * @brief Perform a logical AND operation between two Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the AND operation
+   */
+  static Tristate andGate(Tristate a, Tristate b) noexcept;
 
-}
+  /**
+   * @brief Perform a logical OR operation between two Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the OR operation
+   */
+  static Tristate orGate(Tristate a, Tristate b) noexcept;
+
+  /**
+   * @brief Perform a logical XOR (exclusive OR) operation between two Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the XOR operation
+   */
+  static Tristate xorGate(Tristate a, Tristate b) noexcept;
+
+  /**
+   * @brief Perform a logical NOT operation on a Tristate
+   *
+   * @param a The operand
+   * @return Tristate The result of the NOT operation
+   */
+  static Tristate notGate(Tristate a) noexcept;
+
+  /**
+   * @brief Perform a logical NAND operation between two Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the NAND operation
+   */
+  static Tristate nandGate(Tristate a, Tristate b) noexcept;
+
+  /**
+   * @brief Perform a logical NOR operation between two Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the NOR operation
+   */
+  static Tristate norGate(Tristate a, Tristate b) noexcept;
+
+  /**
+   * @brief Perform a logical XNOR (exclusive NOR) operation between two
+   * Tristates
+   *
+   * @param a First operand
+   * @param b Second operand
+   * @return Tristate The result of the XNOR operation
+   */
+  static Tristate xnorGate(Tristate a, Tristate b) noexcept;
+};
+
+} // namespace nts
 
 #endif /* !NTS_TRISTATELOGIC_HPP_ */
